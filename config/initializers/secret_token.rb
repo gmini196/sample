f2e89206a97d004590bd553bff37999ddaefae38
moonshadow689@gmail.com <framgia@framgia-Inspiron-5537.(none)>
2013-12-12 09:33:16 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'cf2f8ef9c510579d8ee9b5330ca772b484589f41fc20d01c6dd6301d463af9a436edf73098677f794eec9da7eabb3aed98d058e631ac8876c24e324d2d0c9b68'
